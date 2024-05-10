ace8cfff9fcf05e0aa20fc7daacd329c1a891640
Vlad <vladbozhko003@gmail.com>
2024-05-10 17:42:44 +0300

    testing-orm: implemented filtering logs

--- src/services/admin/logs.rs
+++ src/services/admin/logs.rs
@@ -1,25 +1,52 @@
 use crate::entities::logs;
-use actix_web::{get, web, HttpResponse, Responder};
-use sea_orm::{query::*, DatabaseConnection, EntityTrait, TransactionTrait};
+use actix_web::{get, web::{self, Query}, HttpResponse, Responder};
+use sea_orm::{query::*, ColumnTrait, DatabaseConnection, EntityTrait, TransactionTrait};
+use validator::Validate;
 
 
 
 
 #[get("/logs")]
-pub async fn get_admin_logs(pool: web::Data<DatabaseConnection>)-> impl Responder {
+pub async fn get_admin_logs(pool: web::Data<DatabaseConnection>,query: Query<logs::ModelQuery>)-> impl Responder {
     
     let transaction = match pool.begin_with_config(Some(sea_orm::IsolationLevel::Serializable), None).await{
         Ok(dat)=> dat,
         Err(_)=>return HttpResponse::InternalServerError().finish()
     };
-        
+
+    if query.validate().is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+
     let result = logs::Entity::find()
         .select_only()
         .columns([
-            logs::Column::Id,
+            logs::Column::Id,   
             logs::Column::Time,
             logs::Column::Description,
         ])
+        .filter({
+            let time_lowerbound = query.time_lowerbound.to_owned();
+            let time_upperbound = query.time_upperbound.to_owned();
+            let event = query.event.to_owned();
+            
+            let mut cond = Condition::all();
+
+            if let Some(dat) = time_lowerbound{
+                cond = cond.add(logs::Column::Time.gte(dat));
+            }
+
+            if let Some(dat) = time_upperbound{
+                cond = cond.add(logs::Column::Time.lte(dat));
+            }
+
+            if let Some(dat) = event{
+                cond = cond.add(logs::Column::Description.like(format!("%{}%",dat)))
+            }
+            cond
+        })
+        .order_by_desc(logs::Column::Time)
         .into_json()
         .all(&transaction)
         .await;
